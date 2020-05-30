edfb952190276ddf19ae7454a216c34412797a18
James Wallace <4538317+NZLWally@users.noreply.github.com>
2020-05-30 17:42:39 +1200

    Complete User Inputs workings

--- ruby_basics/6_user_input/10.rb
+++ ruby_basics/6_user_input/10.rb
@@ -33,3 +33,83 @@
 # -7 + 5 = -2
 
 
+# Steps:
+# Ask for first user input of integer
+# Check input using valid_number method
+# loop if Invalid
+
+# Ask for second user input of integer
+# Check input using valid_number method
+# loop if Invalid
+
+# Check if one integer positive and the other negative
+# If not, loop for user inputs again
+
+# If so, add the numbers and print the result
+
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
+
+input_1 = nil
+number_1 = nil
+input_2 = nil
+number_2 = nil
+
+loop do
+
+  loop do
+    puts "Enter a positive or negative integer:"
+    input_1 = gets.chomp
+    break if valid_number?(input_1)
+    puts "Invalid input. Only non-zero integers are allowed."
+  end
+  number_1 = input_1.to_i
+
+  loop do
+    puts "Enter a positive or negative integer:"
+    input_2 = gets.chomp
+    break if valid_number?(input_2)
+    puts "Invalid input. Only non-zero integers are allowed."
+  end
+  number_2 = input_2.to_i
+
+  results = [number_1, number_2].sort!
+  if results[0] < 0 && results[1] > 0
+    break
+  else
+    puts "Sorry. One integer must be positive, one must be negative. \nPlease start over."
+  end
+end
+
+result = number_1 + number_2
+puts "#{number_1} + #{number_2} is #{result}"
+
+# or
+
+number_1 = nil
+number_2 = nil
+
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
+
+def request_number
+  loop do
+    puts "Enter a positive or negative integer:"
+    answer = gets.chomp
+    return answer.to_i if valid_number?(answer)
+    puts "Invalid input. Only non-zero integers are allowed."
+  end
+end
+
+loop do
+  number_1 = request_number
+  number_2 = request_number
+
+  break if number_1 * number_2 < 0
+  puts "Sorry. One integer must be positive, one must be negative. \nPlease start over."
+end
+
+result = number_1 + number_2
+puts "#{number_1} + #{number_2} is #{result}"
--- ruby_basics/6_user_input/9.rb
+++ ruby_basics/6_user_input/9.rb
@@ -12,6 +12,7 @@
 #   puts 'Launch School is the best!'
 #   number_of_lines -= 1
 # end
+
 # Modify this program so it repeats itself after each input/print iteration, asking for a new number each time through. The program should keep running until the user enters q or Q.
 
 # Examples:
@@ -35,3 +36,46 @@
 # >> How many output lines do you want? Enter a number >= 3 (Q to quit):
 # q
 
+number_of_lines = nil
+
+loop do
+  puts "How many output lines do you want? Enter a number >= 3 (Q to quit)"
+  number_of_lines = gets.chomp
+  break if number_of_lines.upcase == "Q"
+  number_of_lines = number_of_lines.to_i
+  if number_of_lines >= 3
+    while number_of_lines > 0
+      puts "Launch School is the best!"
+      number_of_lines -= 1
+    end
+  else
+    puts "That's not enough lines."
+  end
+end
+
+# Solution
+
+loop do
+  input_string = nil
+  number_of_lines = nil
+
+  loop do
+    puts '>> How many output lines do you want? ' \
+         'Enter a number >= 3 (Q to Quit):'
+
+    input_string = gets.chomp.downcase
+    break if input_string == 'q'
+
+    number_of_lines = input_string.to_i
+    break if number_of_lines >= 3
+
+    puts ">> That's not enough lines."
+  end
+
+  break if input_string == 'q'
+
+  while number_of_lines > 0
+    puts 'Launch School is the best!'
+    number_of_lines -= 1
+  end
+end
