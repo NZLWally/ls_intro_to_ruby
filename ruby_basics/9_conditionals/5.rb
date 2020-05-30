d1871ab68d255a98c82fd3d3b8a4b4abe390dda7
James Wallace <4538317+NZLWally@users.noreply.github.com>
2020-05-30 21:27:39 +1200

    Complete Conditionals workings

---
+++ ruby_basics/9_conditionals/5.rb
@@ -0,0 +1,11 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# will print "My favourite number is 7" because an integer has a truthy value. Anything except false and nil are truthy.
